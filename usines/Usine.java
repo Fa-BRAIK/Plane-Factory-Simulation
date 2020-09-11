b7fb23d401fb5588684aa909406e3383d951dbc8
Farouk BRAIK <faroukbraik17@gmail.com>
2020-09-11 17:35:00 -0400

    adding EtatUsine enum

--- usines/Usine.java
+++ usines/Usine.java
@@ -1,9 +1,29 @@
 package usines;
 
-import java.awt.*;
+import javafx.scene.image.Image;
+
 import java.util.ArrayList;
 
 public class Usine {
     protected Image icones;
-    protected ArrayList<Entree> entree;
+    protected ArrayList<Entree> entrees;
+    protected Sortie sortie;
+    protected EtatUsine etat= EtatUsine.VIDE;
+
+    Usine(ArrayList<Entree> entrees, Sortie sortie) {
+        this.entrees = entrees;
+        this.sortie = sortie;
+    }
+
+    public Image getIcones() {
+        return icones;
+    }
+
+    public ArrayList<Entree> getEntrees() {
+        return entrees;
+    }
+
+    public Sortie getSortie() {
+        return sortie;
+    }
 }
