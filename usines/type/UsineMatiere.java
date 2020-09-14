9f38d43b09185939e7037c78ca43b7fb8259dfb0
Farouk BRAIK <faroukbraik17@gmail.com>
2020-09-14 00:23:59 -0400

    updating usine matiere

--- usines/type/UsineMatiere.java
+++ usines/type/UsineMatiere.java
@@ -2,7 +2,17 @@ package usines.type;
 
 import usines.UsineProductive;
 
+import java.util.ArrayList;
+
 public class UsineMatiere extends UsineProductive {
+    private static ArrayList<String> entrees = new ArrayList<>();
+    private static String sortie = null;
+
+    public static void init(ArrayList<String> entrees, String sortie) {
+        UsineMatiere.entrees = entrees;
+        UsineMatiere.sortie = sortie;
+    }
+
     public UsineMatiere(int id, double positionX, double positionY, int intervalPoduction) {
         super(id, positionX, positionY, intervalPoduction);
     }
