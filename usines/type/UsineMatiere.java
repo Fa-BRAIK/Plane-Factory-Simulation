4e849c648ff764575c6249ffd1e06ec3f2b837c9
Farouk BRAIK <faroukbraik17@gmail.com>
2020-09-14 01:09:07 -0400

    updating classes

--- usines/type/UsineMatiere.java
+++ usines/type/UsineMatiere.java
@@ -1,19 +1,26 @@
 package usines.type;
 
+import composants.Composant;
 import usines.UsineProductive;
 
 import java.util.ArrayList;
 
 public class UsineMatiere extends UsineProductive {
-    private static ArrayList<String> entrees = new ArrayList<>();
-    private static String sortie = null;
+    private static ArrayList<Composant> entreesUsine = new ArrayList<>();
+    private static Composant sortieUsine = null;
 
-    public static void init(ArrayList<String> entrees, String sortie) {
-        UsineMatiere.entrees = entrees;
-        UsineMatiere.sortie = sortie;
+    public static void init(ArrayList<Composant> entrees, Composant sortie) {
+        UsineMatiere.entreesUsine = entrees;
+        UsineMatiere.sortieUsine = sortie;
     }
 
     public UsineMatiere(int id, double positionX, double positionY, int intervalPoduction) {
         super(id, positionX, positionY, intervalPoduction);
+
+        for (Composant composant : UsineMatiere.entreesUsine) {
+            entrees.add(Composant.nouvelComposant(composant));
+        }
+
+        sortie = Composant.nouvelComposant(UsineMatiere.sortieUsine);
     }
 }
