48b6feb0bf42cbaf64efba52883045063f1cd51c
Farouk BRAIK <faroukbraik17@gmail.com>
2020-09-14 01:11:44 -0400

    adding Chemin class

---
+++ usines/Chemin.java
@@ -0,0 +1,18 @@
+package usines;
+
+public class Chemin {
+    private int de, vers;
+
+    public Chemin(int de, int vers) {
+        this.de = de;
+        this.vers = vers;
+    }
+
+    public int getDe() {
+        return de;
+    }
+
+    public int getVers() {
+        return vers;
+    }
+}
